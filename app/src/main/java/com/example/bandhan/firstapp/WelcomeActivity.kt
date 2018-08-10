15b2d7ecdacceea9f4b463778cfc1158c455549a
priyankama <priyankamaheshwari2697@gmail.com>
2018-08-10 13:02:21 +0530

    sec page added

--- app/src/main/java/com/example/bandhan/firstapp/WelcomeActivity.kt
+++ app/src/main/java/com/example/bandhan/firstapp/WelcomeActivity.kt
@@ -1,12 +1,20 @@
 package com.example.bandhan.firstapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+
+        getstartedbtn.setOnClickListener {
+            val partitionintent = Intent(this,PartitionActivity::class.java)
+            startActivity(partitionintent)
+        }
     }
 }
